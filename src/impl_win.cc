eb497deb07e13f53cc4bfe8ee24281823c7514fe
Cheng Zhao <zcbenz@gmail.com>
2018-12-11 13:51:42 +0900

    Add implementation for Windows

--- src/impl_win.cc
+++ src/impl_win.cc
@@ -1,9 +1,14 @@
 #include "impl.h"
 
+#include <windows.h>
+
 namespace impl {
 
 bool IsValidWindow(char* handle, size_t size) {
-  return true;
+  if (size != sizeof(HWND))
+    return false;
+  HWND window = *reinterpret_cast<HWND*>(handle);
+  return ::IsWindow(window);
 }
 
 }  // namespace impl
