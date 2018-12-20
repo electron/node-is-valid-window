4dbf3dfdbda2d8608b1e9c9d8a40eee962ea3d4d
deepak1556 <hop2deep@gmail.com>
2018-12-21 05:06:00 +0530

    fix: use v8::Local instead of v8::Handle
    the alias is not available when built with V8_IMMINENT_DEPRECATION_WARNINGS

--- src/main.cc
+++ src/main.cc
@@ -13,10 +13,10 @@ void IsValidWindow(const Nan::FunctionCallbackInfo<v8::Value>& info) {
                                                 node::Buffer::Length(info[0])));
 }
 
-void Init(v8::Handle<v8::Object> exports) {
+void Init(v8::Local<v8::Object> exports) {
   Nan::SetMethod(exports, "isValidWindow", IsValidWindow);
 }
 
 }  // namespace
 
-NODE_MODULE(is_valid_window, Init)
+NODE_MODULE(NODE_GYP_MODULE_NAME, Init)
